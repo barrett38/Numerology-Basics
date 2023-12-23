3eb69ab581052def5047e3b790e53d43f5ff259d
Barrett Anderson <barrett1614@gmail.com>
2023-12-23 10:46:45 -0600

    Fixing 'Cannot GET /'

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
 const PORT = 8000;
 const cors = require("cors");
 app.use(cors());
