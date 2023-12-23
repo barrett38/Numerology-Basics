629c0b30dadd9ac9c6a7af22ee4d807673e970b9
Barrett Anderson <barrett1614@gmail.com>
2023-12-23 10:53:40 -0600

    Server.js changed

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello, World!");
-});
-
 const PORT = 8000;
 const cors = require("cors");
 app.use(cors());
